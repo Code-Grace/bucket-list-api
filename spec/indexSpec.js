6550801aa6edb88763482341c2088fcbbe94c08c
DanielJRaine <ley.danielj@gmail.com>
2016-10-14 17:41:26 -0400

    Write hello world test, add script alias to package.json

---
+++ spec/indexSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let chai = require('chai');
+let expect = require('chai').expect;
+
+describe('Sanitize', function() {
+  it('returns lowercase of a string', function() {
+    expect('HELLO WORLD').to.equal('HELLO WORLD');
+  });
+  it('remove any hyphen');
+});
