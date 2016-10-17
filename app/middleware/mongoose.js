a3101368a8bdbe7f89d1eec0da70316a45216131
danieljraine <ley.danielj@gmail.com>
2016-10-17 16:41:15 -0400

    correct NODE_ENV to production

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,14 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI; // || 'mongodb://127.0.0.1/bucket-list-api';
+// const uri = process.env.MONGOLAB_URI; || 'mongodb://127.0.0.1/bucket-list-api';
+let uri;
+if (process.env.NODE_ENV === 'production') {
+  uri = process.env.MONGODB_URI;
+} else {
+  uri = 'mongodb://localhost/bucket-list-api';
+} 
+
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
