4c3b78544361e062bbc6874d02f462dfe956e8e0
danieljraine <ley.danielj@gmail.com>
2016-10-19 10:47:19 -0400

    add query params to find

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -8,7 +8,7 @@ const Task = models.task;
 const authenticate = require('./concerns/authenticate');
 
 const index = (req, res, next) => {
-  Task.find({})
+  Task.find({ _owner: req.currentUser._id })
     .then((tasks) => {
       return res.json({ tasks });
     })
